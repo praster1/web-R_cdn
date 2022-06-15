c4b933b218e1c951e1c7b5b95f3f03418e966449
Jaeseong Yoo <praster1@gmail.com>
2022-06-15 21:02:07 +0900

    requestUUID

---
+++ javascripts/community/ajax_readArticle_20220615_2100.js
@@ -0,0 +1,33 @@
+function ajax_readArticle(requestUUID)
+{
+	url = "/get_readArticle_board_community/?requestUUID=" + requestUUID
+	
+	$.ajax({
+		url : url,
+		success:function(data){
+			console.log(data)
+		
+			// 제목
+			title = data.title.toString()
+			html = '<h2>' + title + '</h2>'
+			$("#title").append(html)
+			changeTitle(title)		// 메타 태그: 타이틀 변경
+			
+			// 작성자
+			html = '<b>작성자: </b>' + data.user_name.toString()
+			$("#username").append(html)
+			
+			// 작성 일시
+			html = '<b>작성 일시:</b> ' + data.created_at.toString()
+			$("#created_at").append(html)
+			
+			// 조회수
+			html = "조회 수: " + data.details.readed_count.toString() + "건"
+			$("#viewer_count").append(html)
+			
+			// 내용
+			html = data.content.toString()
+			$("#content").append(html)
+		}
+	})
+}
