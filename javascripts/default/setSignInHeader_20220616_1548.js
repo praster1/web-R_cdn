d4d785e62dd0f6ddb49db013199cfa67285c3c1a
Jaeseong Yoo <praster1@gmail.com>
2022-06-16 15:48:50 +0900

    setSignInHeader

---
+++ javascripts/default/setSignInHeader_20220616_1548.js
@@ -0,0 +1,28 @@
+function setSignInHeader() {
+	$.ajax({
+		url : "/get_user_info/",
+		success:function(data){		
+			$("#userinfo_role").html("")
+			$("#index_userinfo_role").html("")
+			
+			html = data.group_info.group_name.toString() + " 계정입니다."
+			$("#userinfo_role").append(html)
+			
+			if (data.group_info.uuid.toString() == "961f0790-d788-11ec-9d64-0242ac120002")
+			{
+				$("#myInfo_nonauthorized").show()
+			}
+			
+			html = "<b>회원 등급:</b> " + data.group_info.group_name.toString()
+			$("#index_userinfo_role").append(html)
+
+			if ("is_admin" in data.group_info || "is_staff" in data.group_info )
+			{
+				 if (data.group_info.is_admin > 0 || data.group_info.is_staff > 0)
+				{
+					$("#notification_operation").show()
+				}
+			}
+		}
+	})
+}
