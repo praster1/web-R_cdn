baf16c21b4ec11a8d9778ca1fb420c6ba2458dd7
Jaeseong Yoo <praster1@gmail.com>
2022-06-18 17:21:55 +0900

    ajax_readArticle_20220618_1721

---
+++ javascripts/community/ajax_readArticle_20220618_1721.js
@@ -0,0 +1,39 @@
+function ajax_readArticle(requestUUID)
+{
+	url = "/get_readArticle_board_community/?requestUUID=" + requestUUID
+	
+	$.ajax({
+		url : url,
+		success:function(data){
+			console.log(data)
+			
+			// 버튼 목록
+			$("#buttonList").append(data.button_list.toString())
+		
+			// 첨부 파일
+			$("#attachedFile").append(data.attached_file.toString())
+		
+			// 제목
+			title = data.title.toString()
+			html = '<h2>' + title + '</h2>'
+			$("#title").append(html)
+			changeTitle(title)		// 메타 태그: 타이틀 변경
+			
+			// 작성자
+			html = '<b>작성자: </b>' + data.user_name.toString()
+			$("#username").append(html)
+			
+			// 작성 일시
+			html = '<b>작성 일시:</b> ' + data.created_at.toString()
+			$("#created_at").append(html)
+			
+			// 조회수
+			html = "조회 수: " + data.details.readed_count.toString() + "건"
+			$("#viewer_count").append(html)
+			
+			// 내용
+			html = data.content.toString()
+			$("#content").append(html)
+		}
+	})
+}
