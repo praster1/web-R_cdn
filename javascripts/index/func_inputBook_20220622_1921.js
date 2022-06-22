f9bed1f5bb044131e4ef531fb14fb3cb8d24d226
Jaeseong Yoo <praster1@gmail.com>
2022-06-22 19:22:13 +0900

    로딩중

--- javascripts/index/func_inputBook_20220622_1921.js
+++ javascripts/index/func_inputBook_20220622_1921.js
@@ -1,20 +1,20 @@
-function func_inputYoutube(channelUUID) {
-	var url = "/get_index_youtube/?uuid=" + channelUUID + "&videoCount=1"
+function func_inputBook() {
+	var url = "/get_index_book/?videoCount=1"
 	
 	$.ajax({
 		url : url,
 		success:function(data){
-		
+			console.log(data)
 			for(key in data){
-				$("#index_youtube").html("")
+				$("#index_book").html("")
 				
 				html = ""
 				
 				html += '<div class="blog-img">'
 				
 				html += '<center>'
-				html += '<a class="thumb-image" href="' + data[key].url + '" target="_blank">'
-				html += '<img src="' + data[key].url_thumbnail + '" alt="" width="75%"/>'
+				html += '<a class="thumb-image" href="' + data[key].link + '" target="_blank">'
+				html += '<img src="' + data[key].image + '" alt="" />'
 				html += '</a>'
 				html += '</center>'
 				
@@ -25,15 +25,16 @@ function func_inputYoutube(channelUUID) {
 				
 				html += '<div class="blog-hd-sw">'
 				html += '<center>'
-				html += '<a class="thumb-image" href="' + data[key].url + '" target="_blank">'
+				html += '<a class="thumb-image" href="' + data[key].link + '" target="_blank">'
 				html += '<h2>' + data[key].title + '</h2>'
 				html += '</a>'
 				html += '</center>'
 				html += '</div>'
+				html += '<p>' + textLengthOverCut(data[key].description.toString(), 85) + '</p>'
 
 				html += '</div>'
 				
-				$("#index_youtube").append(html)
+				$("#index_book").append(html)
 			}
 		}
 	})
