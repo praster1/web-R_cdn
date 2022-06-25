5680158260fd6a7e33f88108325e4c7b9a735057
Jaeseong Yoo <praster1@gmail.com>
2022-06-25 20:46:48 +0900

    setSignInHeader_20220625_2046

--- javascripts/default/setSignInHeader_20220625_2046.js
+++ javascripts/default/setSignInHeader_20220625_2046.js
@@ -5,15 +5,20 @@ function setSignInHeader() {
 			$("#userinfo_role").html("")
 			$("#index_userinfo_role").html("")
 			
-			html = data.group_info.group_name.toString() + " 계정입니다."
+			// 계정 등급
+			html = data.group_info.group_name.toString() + " 계정입니다.<br/>"
 			
-			expiredAt = data.group_info.expired_at
+			// 만료일
+			expiredAt = data.info_WebR.expired_at
+			html += "<span style='color:MediumBlue'>"
 			if (expiredAt == "None")
 			{
 				html += "만료일: 영구 적용"
 			} else {
 				html += "만료일: " + expiredAt.toString()
 			}
+			html += "</span>"
+			
 			$("#userinfo_role").append(html)
 			
 			if (data.group_info.uuid.toString() == "961f0790-d788-11ec-9d64-0242ac120002")
